49d34121c0f198e54aa85e75dc143a5357ad0891
wakenedo <a_nuner@outlook.com>
2023-05-25 20:34:19 -0300

    exercise 6 bug fixed

--- javaScript/js/idealWeight.js
+++ javaScript/js/idealWeight.js
@@ -1,23 +1,36 @@
 function calculateIdealWeight() {
   // Get input values
-  let sex = document.getElementById('sex').value;
-  let height = parseFloat(document.getElementById('height').value);
-
-  // Calculate ideal weight
-  let idealWeight;
-  if (sex === 'male') {
-    idealWeight = (72.7 * height / 100) - 58;
-  } else if (sex === 'female') {
-    idealWeight = (62.1 * height / 100) - 44.7;
-  } else {
-    idealWeight = NaN;
+  let selectedSex = document.querySelector('input[name="sex"]:checked').value;
+  let heightInput = parseFloat(document.getElementById('_height').value); // Convert height to meters
+  if (isNaN(heightInput)) {
+    heightInput = 0; // Set height to 0 if it's not a valid number
   }
 
-  // Display ideal weight
-  let idealWeightElement = document.getElementById('idealWeight');
-  if (!isNaN(idealWeight)) {
-    idealWeightElement.textContent = `Ideal Weight: ${idealWeight.toFixed(2)} kg`;
+  console.log(selectedSex, heightInput)
+  // Check if sex and height are entered
+  if (selectedSex !== undefined && heightInput !== 0) {
+    let sex = selectedSex.toLowerCase();
+
+    // Calculate ideal weight
+    let idealWeight;
+    if (sex === 'male') {
+      idealWeight = (0.5 * heightInput) - 11.5;
+    } else if (sex === 'female') {
+      idealWeight = (0.45 * heightInput) - 9.25;
+    } else {
+      idealWeight = NaN;
+    }
+
+    // Display ideal weight
+    let idealWeightElement = document.getElementById('idealWeight');
+    if (!isNaN(idealWeight)) {
+      idealWeightElement.textContent = `Ideal Weight: ${idealWeight.toFixed(2)} kg`;
+    } else {
+      idealWeightElement.textContent = 'Please select a valid sex.';
+    }
   } else {
-    idealWeightElement.textContent = 'Please select a valid sex.';
+    // Display error message if inputs are missing
+    let idealWeightElement = document.getElementById('idealWeight');
+    idealWeightElement.textContent = 'Please select sex and enter height.';
   }
 }
